a01ea42bdf8ab868edfc8c1efe87c4768fe6d787
mwamba <mwambachanda51@gmail.com>
2026-01-12 23:36:00 -0500

    Init commit, first init

---
+++ Clorah-Hospital/src/main/java/com/clorah/Clorah/Hospital/ClorahHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.clorah.Clorah.Hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClorahHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClorahHospitalApplication.class, args);
+	}
+
+}
---
+++ Clorah-Hospital/src/test/java/com/clorah/Clorah/Hospital/ClorahHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clorah.Clorah.Hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClorahHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
